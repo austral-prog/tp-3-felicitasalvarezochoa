348a96385322c76fb1456f0cc1cc0c6f1a8872f4
felicitasalvarezochoa <falvarezochoa@mail.austral.edu.ar>
2025-04-08 14:54:47 +0000

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -4,7 +4,5 @@ def slice_simple():
     print(texto[0:3:1])
     print(texto[2:5:1])
     print(texto[0:4]+texto[-3:])
-
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
